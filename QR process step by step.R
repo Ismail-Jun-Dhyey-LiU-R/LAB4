009fb62d2721a5c733159d22ff9dba4b9197259b
Ismail Khalil <ismailkhalil2@gmail.com>
2019-09-26 14:27:15 +0100

    Finnaly

--- QR process step by step.R
+++ QR process step by step.R
@@ -1,41 +1,33 @@
-my.QR <- function(y, X) {
-  X <- as.matrix(X)
-  y <- as.vector(y)
+
+library(iris)
+QR <- function(X,y){
+  # Create response vector y
+  y <- iris$Sepal.Length
+  
+  # Matrix of feature variables from Iris
+  X <- as.matrix(iris[-ncol(iris)])
+  
+  # vector of ones with same length as rows in Iris
+  int <- rep(1, length(y))
+  
+  # Add intercept column to X
+  X <- cbind(int, X)
   n_col <- as.integer(ncol(X))
   
   n_row <- as.integer(nrow(X))
+  # Implement closed-form solution
+  betas <- solve(t(X) %*% X) %*% t(X) %*% y
   
-  X <- qr(X)
-  beta <- solve(t(X) %*% X, t(X) %*% y)
-  e <- as.vector(y - X %*% beta) 
+  # Round for easier viewing
+  betas <- round(betas, 2)
+  betas
+  
+  e <- as.vector(y - X %*% betas)
+  e
   R2 <- (X[1:n_col, 1:n_col])^2
-  return(list(beta = beta, e= e, R2 = R2 ))
+  R2
+  return(list(betas = betas, e= e, R2 = R2 ))
 }
 
-X <- iris
+QR(X,y)
 
-
-y <- iris$Sepal.Length
-
-my.QR(y, X)
-
-
-
-
-f2 <- function (X, y) {
-  ## QR factorization `X = QR`
-  QR <- qr.default(X)
-  ## After rotation of `X` and `y`, solve upper triangular system `Rb = Q'y` 
-  b <- backsolve(QR$qr, qr.qty(QR, y))
-  ## residuals
-  e <- as.numeric(y - X %*% b)
-  ## R-squared
-  RSS <- crossprod(e)[1]
-  TSS <- crossprod(y - mean(y))[1]
-  R2 <- 1 - RSS / TSS
-  ## multiple return
-  list(coefficients = b, residuals = e, R2 = R2)
-}
-
-
-f2(X,y)
